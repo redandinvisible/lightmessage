a9da4702c7cf5cea4c27afd9c4c730b802019be5
Rachel Wilson <rachel.wilson@theodi.com>
2019-06-02 20:57:21 +0100

    testing of node-rpi-rgb-led-matrix library

--- routes/index.js
+++ routes/index.js
@@ -5,23 +5,24 @@ router.get('/', function (req, res, next) {
     res.render('index');
 });
 
-router.post('/', function (req, res, next) {
-    console.log(req.query.message);
-    var Matrix = require("easybotics-rgb-led-matrix");
-    led = new Matrix(32, 64, 1, 1, 100, "adafruit-hat"); //this might be different for you
-
-    const input = "hello world!"; //wherever you get the input from
-    const font  =  __dirname + '/fonts/' + "5x8.bdf";
-
-    //should be a function that calculates the postition based on timestamp
-    //and increments the x position
-    var x = 0;
-    var y = 0;
-
-    led.drawText(x, y, 255, 255, 255, input, font);
-    led.update();
-    while(true){};
-    res.end();
-});
+// router.post('/', function (req, res, next) {
+    // console.log('test page:' + req.query.message);
+    // var Matrix = require("easybotics-rgb-led-matrix");
+    // led = new Matrix(32, 64, 1, 1, 100, "adafruit-hat"); //this might be different for you
+    //
+    // const input = "hello world!"; //wherever you get the input from
+    // console.log("dirname" + __dirname);
+    // const font  =  __dirname + '/fonts/' + "5x8.bdf";
+    //
+    // //should be a function that calculates the position based on timestamp
+    // //and increments the x position
+    // var x = 0;
+    // var y = 0;
+    //
+    // led.drawText(x, y, 255, 255, 255, input, font);
+    // led.update();
+    // while(true){};
+    // res.end();
+// });
 
 module.exports = router;
--- routes/lightmessage.js
+++ routes/lightmessage.js
@@ -2,8 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/', function(req, res, next) {
-  console.log('light up!' + req.query.message);
-  res.render('index', { last_message: req.query.message });
+  let msg = req.query.message ? req.query.message : req.body.message;
+  console.log('light up!' + msg);
+
+  var Matrix = require("easybotics-rgb-led-matrix");
+  led = new Matrix(32, 64, 1, 1, 100, "adafruit-hat"); //this might be different for you
+
+  const input = "hello world!"; //wherever you get the input from
+  console.log("dirname" + __dirname);
+  const font  =  __dirname + '/fonts/' + "5x8.bdf";
+
+  //should be a function that calculates the position based on timestamp
+  //and increments the x position
+  var x = 0;
+  var y = 0;
+
+  led.drawText(x, y, 255, 255, 255, input, font);
+  led.update();
+  while(true){};
+  res.end();
+
+  res.render('index', { last_message: req.query.message ? req.query.message : req.body.message });
+  res.end();
 });
 
 module.exports = router;
